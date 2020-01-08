1aa86b03480a59bbd700d6bd2e94ec4786a669dc
webmarkyn <web.markyn@gmail.com>
2020-01-08 21:25:51 +0200

    fixed eslint errors in header module

--- src/header.component.js
+++ src/header.component.js
@@ -1,6 +1,6 @@
 export default function header() {
-  const header = document.createElement("header");
-  header.style.cssText = `
+  const headerEl = document.createElement('headerEl');
+  headerEl.style.cssText = `
         width: 100%;
         height: 400px;
         background: url('./assets/header.jpg');
@@ -10,12 +10,12 @@ export default function header() {
         align-items: center;
         color: aliceblue;
     `;
-  const h1 = document.createElement("h1");
-  h1.innerText = "The best Restaurant"
+  const h1 = document.createElement('h1');
+  h1.innerText = 'The best Restaurant';
   h1.style.cssText = `
         font-family: 'Trebuchet MS', Arial, sans-serif;
         font-size: 43px;
     `;
-  header.appendChild(h1);
-  return header;
+  headerEl.appendChild(h1);
+  return headerEl;
 }
