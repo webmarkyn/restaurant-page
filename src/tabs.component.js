3f253336c7b8f71149859afbdd7248964dddae43
webmarkyn <web.markyn@gmail.com>
2020-01-09 18:01:45 +0200

    fixed tabs module linting erros

--- src/tabs.component.js
+++ src/tabs.component.js
@@ -1,6 +1,6 @@
 export default function tabs() {
   const tabsEl = document.createElement('div');
-  tabsEl.id = 'tabs'
+  tabsEl.id = 'tabs';
   const tabsNav = document.createElement('div');
   const contacts = document.createElement('a');
   contacts.innerText = 'Contacts';
@@ -13,8 +13,8 @@ export default function tabs() {
   tabsNav.classList.add('tabs-nav');
   tabsEl.appendChild(tabsNav);
   const currentContent = document.createElement('div');
-  currentContent.classList.add('tab')
-  currentContent.id = 'currentTab'
-  tabsEl.appendChild(currentContent)
+  currentContent.classList.add('tab');
+  currentContent.id = 'currentTab';
+  tabsEl.appendChild(currentContent);
   return tabsEl;
 }
